63f9fe8c6e36b89d4c683957eccee226fd8a90e0
Arya Rahmanian <aryarahmanian@tamu.edu>
2022-04-11 00:18:34 -0500

    working  on national park api

---
+++ client/js/functions.js
@@ -0,0 +1,36 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() *( max ));
+}
+
+//function generateParkInfo(){
+//    var varState= document.getElementById("inputState1").value;
+//    const state = new String(varState);
+
+    var iteminJSON = 0;
+    var request = require('request');
+
+    var options = {
+    'method': 'GET',
+    'url': 'https://developer.nps.gov/api/v1/parks?q=camping&api_key=clKNqgX4H1lU7WEsGuUOkJxbKEyEFPoL6tXRDBEu&stateCode=OR',
+    'headers': {
+        'api_key': 'clKNqgX4H1lU7WEsGuUOkJxbKEyEFPoL6tXRDBEu'
+      },
+      json:true
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error); 
+     //   console.log(response.body)
+        if(response.body.limit > response.body.total ){
+            iteminJSON = getRandomInt(response.body.total );
+           
+        }
+        else{
+            iteminJSON = getRandomInt(response.body.limit );
+            
+        }
+        console.log(response.body.data[iteminJSON]);
+    });
+
+   
+    
+//}
