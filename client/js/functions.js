43b8137124da25bf3589a42cb4af690a65d69510
ans <ansleythomp@gmail.com>
2022-04-12 13:43:57 -0500

    change button spacing

--- client/js/functions.js
+++ client/js/functions.js
@@ -2,9 +2,9 @@ function getRandomInt(max) {
     return Math.floor(Math.random() *( max ));
 }
 
-//function generateParkInfo(){
-//    var varState= document.getElementById("inputState1").value;
-//    const state = new String(varState);
+function generateParkInfo(){
+   var varState= document.getElementById("inputState1").value;
+   // const state = new String(varState);
 
     var iteminJSON = 0;
     var request = require('request');
@@ -18,19 +18,35 @@ function getRandomInt(max) {
       json:true
     };
     request(options, function (error, response) {
-        if (error) throw new Error(error); 
+        if (error) throw new Error(error);
      //   console.log(response.body)
         if(response.body.limit > response.body.total ){
             iteminJSON = getRandomInt(response.body.total );
-           
+            document.write("1");
+
         }
         else{
             iteminJSON = getRandomInt(response.body.limit );
-            
+            document.write("2");
         }
         console.log(response.body.data[iteminJSON]);
     });
 
-   
-    
-//}
+  }
+
+  function displayPark() {
+  // document.display("hello");
+  // const park = data.name;
+  // const heading = document.createElement("h1");
+  // heading.innerHTML = park;
+
+  var options = {
+  'method': 'GET',
+  'url': 'https://developer.nps.gov/api/v1/parks?q=camping&api_key=clKNqgX4H1lU7WEsGuUOkJxbKEyEFPoL6tXRDBEu&stateCode=OR',
+  'headers': {
+      'api_key': 'clKNqgX4H1lU7WEsGuUOkJxbKEyEFPoL6tXRDBEu'
+    },
+    json:true
+  };
+
+}
