5386a633c11dd82904dd29e6f473eaed1ffe7bcd
aryarahmanian <aryarahmanian@tamu.edu>
2022-04-14 13:27:19 -0500

    finally got api info to show in browser

--- client/js/functions.js
+++ client/js/functions.js
@@ -1,34 +1,23 @@
+
+
 function getRandomInt(max) {
     return Math.floor(Math.random() *( max ));
 }
 
-//function generateParkInfo(){
-//    var varState= document.getElementById("inputState1").value;
-//    const state = new String(varState);
+function saveParkinfo(){
+    var varState = document.getElementById("inputState1").value; 
+    const state = new String(varState);
+    console.log(state);
+    localStorage.setItem("state", state);    
+}
 
-    var iteminJSON = 0;
-    var request = require('request');
 
-    var options = {
-    'method': 'GET',
-    'url': 'https://developer.nps.gov/api/v1/parks?q=camping&api_key=clKNqgX4H1lU7WEsGuUOkJxbKEyEFPoL6tXRDBEu&stateCode=TX',
-    'headers': {
-        'api_key': 'clKNqgX4H1lU7WEsGuUOkJxbKEyEFPoL6tXRDBEu'
-      },
-      json:true
-    };
-    request(options, function (error, response) {
-        if (error) throw new Error(error); 
-         
-       // console.log(response.body)
-         if(parseInt(response.body.limit) > parseInt(response.body.total) ){
-            iteminJSON = getRandomInt(response.body.total );    
-        }
-        else{
-            iteminJSON = getRandomInt(response.body.limit );
-        
-        }
-       console.log(response.body.data[iteminJSON]); 
-    });
+function displayParkInformation(data){
+    console.log(data);
+    const nameDiv = document.getElementById("parkName");
+    const parkName = data.fullName;
+    const heading = document.createElement("h1");
+    heading.innerHTML = parkName;
+    nameDiv.appendChild(heading);
 
-//}
+}
